2a7ee41e295112b93091fb61640733fa7bfe4858
mr-rampage <readonly@che.wbac.ca>
2016-05-02 23:13:59 -0400

    Updating formatting

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -1,6 +1,5 @@
- import Rx from "rx/dist/rx.all";
- import {default as counterComponentFactory} from "./component/counter";
+import Rx from "rx/dist/rx.all";
+import {default as counterComponentFactory} from "./component/counter";
 
- var counter = counterComponentFactory(0);
- Rx.Observable.interval(1000).subscribe(counter.increment);
-
+var counter = counterComponentFactory(0);
+Rx.Observable.interval(1000).subscribe(counter.increment);
